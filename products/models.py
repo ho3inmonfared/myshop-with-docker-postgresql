1e32cee40e9999f6aec70834dd7895b103b8e851
Hossein_Monfared <140837105+ho3inmonfared@users.noreply.github.com>
2026-02-25 19:36:52 +0330

    update product 's status

--- products/models.py
+++ products/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 class Product(models.Model):
+    
+    STATUS_CHOICES = (
+        (True,'موجود', ),
+        (False,'ناموجود', )
+    )
+    
     title=models.CharField(max_length=255)
     description=models.TextField()
     price=models.DecimalField(max_digits=10, decimal_places=0)
-    status=models.BooleanField(default=True)
+    status=models.BooleanField(default=True, choices=STATUS_CHOICES)
     
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
