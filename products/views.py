c5f308f1b75d8c5b49d9a97a946fc2df8040419d
Hossein_Monfared <140837105+ho3inmonfared@users.noreply.github.com>
2026-02-25 20:16:22 +0330

    product list and detail with CBV

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from . import models
+
+class ProductList(generic.ListView):
+    queryset=models.Product.objects.filter(status=True)
+    template_name='products/product_list.html'
+    context_object_name='products'
+    
+class ProductDetail(generic.DetailView):
+    model=models.Product
+    template_name='products/product_detail.html'
+    context_object_name='product'
+    
+    
+    
